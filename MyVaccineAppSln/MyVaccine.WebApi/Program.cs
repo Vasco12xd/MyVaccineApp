293b3b809f5109c4873644930224c493edf3b894
vasco12xd <evasco.27@hotmail.com>
2026-05-26 20:32:52 -0500

    Commit inicial después del fork

--- MyVaccineAppSln/MyVaccine.WebApi/Program.cs
+++ MyVaccineAppSln/MyVaccine.WebApi/Program.cs
@@ -1,17 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using MyVaccine.WebApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MyVaccineAppDbContext>(options =>
+    options.UseSqlServer("Server=localhost,14330;Database=MyVaccineAppDb;User Id=sa;Password=Abc.123456;TrustServerCertificate=True;"/*Environment.GetEnvironmentVariable("MyVaccineDbConnectionString"))*/));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
